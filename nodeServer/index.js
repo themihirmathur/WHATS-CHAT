c42a17c26bc0dc698ba03b4de2e31e4298d6b216
Mihir Mathur <92594107+themihirmathur@users.noreply.github.com>
2023-06-11 20:32:54 +0530

    Committed

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,21 +1,30 @@
 //Node server which will handle socket.io connections
-const io = require('socket.io')(8000)
+const server = require('http').createServer();
+const io = require('socket.io')(server);
 
 const users = {};
 
-io.on('connection', socket =>{
-    // If a new user joins, let other users connected to the server know!
-    socket.on('new-user-joined', name =>{ 
-        users[socket.id] = name;
-        socket.broadcast.emit('user-joined', name);
-    });
-    // If someone sends a message, show it to all the other users joined!
-    socket.on('send', message =>{
-        socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
-    });
-    // If someone leaves the chat, let others know!
-    socket.on('disconnect', message =>{
-        socket.broadcast.emit('left', users[socket.id]);
-        delete users[socket.id]
-    });
-})
+io.on('connection', socket => {
+  // If a new user joins, let other users connected to the server know!
+  socket.on('new-user-joined', name => {
+    users[socket.id] = name;
+    socket.broadcast.emit('user-joined', name);
+  });
+
+  // If someone sends a message, show it to all the other users joined!
+  socket.on('send', message => {
+    socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
+  });
+
+  // If someone leaves the chat, let others know!
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('left', users[socket.id]);
+    delete users[socket.id];
+  });
+});
+
+const port = process.env.PORT || 8000;
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
